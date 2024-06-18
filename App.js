90991db79968e1d58a80ac45e1f7323868712cc8
Muhamad Farid Kharismawan <faridkharismawan@gmail.com>
2024-06-18 20:37:23 +0700

    feat: initialize user interface

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import WeatherInfo from './src/components/WeatherInfo';
+import WeatherSearch from './src/components/WeatherSearch';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WeatherSearch />
+      <WeatherInfo />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
-});
+})
+
+export default App;
